5db91b90fc8baa5d77c5d3e482ea8d1a282eb437
MCEBISI MKHOHLIWE <mcebisimkhohliwe@gmail.com>
2018-07-04 08:17:44 +0200

    2018-07-04 solution

---
+++ 2018-07-04/StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+
+            var delimiter = new char[] { ',', '\n' }.ToList();
+            if (numbers.StartsWith("//"))
+            {
+                delimiter.Add(char.Parse(numbers.Substring(2, 1)));
+                numbers = numbers.Substring(numbers.IndexOf('\n') + 1);
+            }
+
+            var stringNumber = numbers.Split(delimiter.ToArray());
+            var negatives = stringNumber.Where(number => int.Parse(number) < 0);
+            if (negatives.Count() > 0)
+            {
+                HandleNegativeNumbers(negatives);
+            }
+            return stringNumber.Where(number => int.Parse(number) <= 1000).Sum(number => int.Parse(number));
+        }
+
+        private static void HandleNegativeNumbers(IEnumerable<string> negatives)
+        {
+                var negativesNumbers = string.Join(" ", negatives.ToArray());
+                throw new Exception($"Negatives not allowed {negativesNumbers}");
+        }
+    }
+}
---
+++ 2018-07-04/StringCalculatorKata/StringCalculatorKata/StringCalculatorTests.cs
@@ -0,0 +1,124 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Add_GivenInvalidString_ShouldReturnZero(string numbers)
+        {
+            //----------------Arrange------------------------
+            var stringCalculator = CreateStringCalculator();
+
+            //----------------Act----------------------------
+            var actual = stringCalculator.Add(numbers);
+
+            //----------------Assert-------------------------
+            var expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1",1)]
+        [TestCase("10",10)]
+        [TestCase("100",100)]
+        public void Add_GivenSingleNumberAsString_ShouldReturnThatNumberAsInteger(string numbers,int expected)
+        {
+            //----------------Arrange------------------------
+            var stringCalculator = CreateStringCalculator();
+
+            //----------------Act----------------------------
+            var actual = stringCalculator.Add(numbers);
+
+            //----------------Assert-------------------------
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("10,78,100", 188)]
+        [TestCase("100,20,50,30", 200)]
+        public void Add_GivenNumbersAsStringSeparatedByComma_ShouldReturnSumOfThoseNumbersAsInteger(string numbers, int expected)
+        {
+            //----------------Arrange------------------------
+            var stringCalculator = new StringCalculator();
+
+            //----------------Act----------------------------
+            var actual = stringCalculator.Add(numbers);
+
+            //----------------Assert-------------------------
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1\n2,3", 6)]
+        [TestCase("10\n78,100\n100", 288)]
+        [TestCase("100\n20,50\n30,50", 250)]
+        public void Add_GivenNumbersAsStringSeparatedByCommaAndNewline_ShouldReturnSumOfThoseNumbersAsInteger(string numbers, int expected)
+        {
+            //----------------Arrange------------------------
+            var stringCalculator = CreateStringCalculator();
+
+            //----------------Act----------------------------
+            var actual = stringCalculator.Add(numbers);
+
+            //----------------Assert-------------------------
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("//;\n1;2", 3)]
+        [TestCase("//^\n10^78^100^100", 288)]
+        [TestCase("//*\n100*20*50*30*50", 250)]
+        public void Add_GivenNumbersAsStringSeparatedBySpecifiedDelimiter_ShouldReturnSumOfThoseNumbersAsInteger(string numbers, int expected)
+        {
+            //----------------Arrange------------------------
+            var stringCalculator = CreateStringCalculator();
+
+            //----------------Act----------------------------
+            var actual = stringCalculator.Add(numbers);
+
+            //----------------Assert-------------------------
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("-8", "Negatives not allowed -8")]
+        [TestCase("-8,100,-9", "Negatives not allowed -8 -9")]
+        [TestCase("//%\n-8%9%-11,45", "Negatives not allowed -8 -11")]
+        public void Add_GivenStringOfNumbersContainingNegativeNumber_ShouldThrowExceptionAllNegativeNumbersThatWerePassed(string numbers, string expected)
+        {
+            //-------------------Arrange--------------------
+            var stringCalculator = CreateStringCalculator();
+
+            //-------------------Act------------------------
+            var actual = Assert.Throws<Exception>(() => stringCalculator.Add(numbers));
+
+            //-------------------Assert---------------------
+            Assert.AreEqual(expected, actual.Message);
+        }
+
+        [TestCase("//;\n2;1001", 2)]
+        [TestCase("10\n78,1010\n100", 188)]
+        [TestCase("100,20,8888,30,50", 200)]
+        public void Add_GivenNumbersAsStringHavingNumberGreaterThan1000SeparatedByDelimiter_ShouldReturnSumOfTheNumbersNotGreaterThan1000(string numbers, int expected)
+        {
+            //----------------Arrange------------------------
+            var stringCalculator = CreateStringCalculator();
+
+            //----------------Act----------------------------
+            var actual = stringCalculator.Add(numbers);
+
+            //----------------Assert-------------------------
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static StringCalculator CreateStringCalculator()
+        {
+            return new StringCalculator();
+        }
+    }
+}
